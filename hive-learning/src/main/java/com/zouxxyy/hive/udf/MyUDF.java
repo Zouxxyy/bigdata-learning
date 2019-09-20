6d501e39ea136312b851635211508481935dc3d1
zouxxyy <932089990@qq.com>
2019-09-20 19:43:04 +0800

    added in 0920

---
+++ hive-learning/src/main/java/com/zouxxyy/hive/udf/MyUDF.java
@@ -0,0 +1,21 @@
+package com.zouxxyy.hive.udf;
+
+import org.apache.hadoop.hive.ql.exec.UDF;
+
+/**
+ * 自定义函数：一进一出
+ */
+
+public class MyUDF extends UDF {
+
+    public int evaluate(int data) {
+
+        return data + 5;
+    }
+
+    // 支持重载
+    public int evaluate(int data, int data2) {
+
+        return data + data2 + 5;
+    }
+}
